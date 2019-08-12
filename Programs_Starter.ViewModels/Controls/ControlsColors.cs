0cd9ae53982ccfb78597fc53a113fd3fc06aaa8e
Konrad Romanowski <k.romanowski.x@wp.pl>
2019-08-12 09:56:04 +0200

    * Added Infragistics Metro Light Theme to the WPF Client * Created ControlColor static class for ViewModel

---
+++ Programs_Starter.ViewModels/Controls/ControlsColors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs_Starter.ViewModels.Controls
+{
+    public static class ControlsColors
+    {
+        public const string RED = "RED";
+        public const string BLACK = "BLACK";
+        public const string ORANGE = "ORANGE";
+        public const string BLUE = "BLUE";
+    }
+}
