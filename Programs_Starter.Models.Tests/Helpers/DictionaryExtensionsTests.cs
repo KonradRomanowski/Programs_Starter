fcdbab0b4a287263e121e4904f146031aee98f53
Konrad Romanowski <k.romanowski.x@wp.pl>
2019-09-23 20:11:05 +0200

    * Added Dictionary Extensions class with Insert method, * Added tests for Dictionary Extensions

---
+++ Programs_Starter.Models.Tests/Helpers/DictionaryExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Programs_Starter.Models.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs_Starter.Models.Tests.Helpers
+{
+    [TestFixture]
+    class DictionaryExtensionsTests
+    {
+        [TestCase]
+        public void InsertTest1()
+        {
+            Dictionary<int, ProgramToStart> testDict = new Dictionary<int, ProgramToStart>();
+            ProgramToStart program1 = new ProgramToStart("test1", "D:\test1.txt");
+            testDict.Add(1, program1);
+            ProgramToStart program2 = new ProgramToStart("test2", "D:\test2.txt");
+            testDict.Add(2, program2);
+            ProgramToStart program3 = new ProgramToStart("test3", "D:\test3.txt");
+            testDict.Add(3, program3);
+
+            ProgramToStart testProgram = new ProgramToStart("testProgram", "D:\testProgram.txt");
+            testDict.Insert(testProgram, 2);
+
+            Assert.AreEqual(4, testDict.Count);
+            Assert.AreEqual(testProgram, testDict[2]);
+            Assert.AreEqual(program2, testDict[3]);
+            Assert.AreEqual(program3, testDict[4]);
+        }
+    }
+}
---
+++ Programs_Starter.Models/Helpers/DictionaryExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programs_Starter.Models.Helpers
+{
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Inserts ProgramToStart with given index as Key, increments all key which are equal or greater
+        /// </summary>
+        /// <param name="dict">Dictionary with programs to start</param>
+        /// <param name="program">New program to be insterted into dictionary</param>
+        /// <param name="index">index on which new program should be inserted</param>
+        public static void Insert(this Dictionary<int, ProgramToStart> dict, ProgramToStart program, int index)
+        {
+            if (program == null || dict == null)
+                throw new ArgumentNullException();
+            if (index <= 0)
+                throw new ArgumentException("Index below or equal 0!");
+            
+
+            List<KeyValuePair<int, ProgramToStart>> tempList = new List<KeyValuePair<int, ProgramToStart>>();
+
+            foreach (var item in dict.OrderBy(x => x.Key))
+            {
+                if (item.Key >= index)
+                {
+                    tempList.Add(item);
+                    dict.Remove(item.Key);
+                }
+            }
+
+            dict.Add(index, program);
+
+            foreach (var item in tempList.OrderBy(x => x.Key))
+            {
+                dict.Add(item.Key + 1, item.Value);
+            }
+
+            tempList.Clear();
+        }
+    }
+}
