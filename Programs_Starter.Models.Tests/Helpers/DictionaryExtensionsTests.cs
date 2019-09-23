305f83a912c2ad8bd7648ecf0a0f531eb33f6723
Konrad Romanowski <k.romanowski.x@wp.pl>
2019-09-23 21:26:04 +0200

    * Added extension method RemoveProgramToStart to Dictionary Extensions * Added test for new extension method

--- Programs_Starter.Models.Tests/Helpers/DictionaryExtensionsTests.cs
+++ Programs_Starter.Models.Tests/Helpers/DictionaryExtensionsTests.cs
@@ -10,7 +10,7 @@ namespace Programs_Starter.Models.Tests.Helpers
     class DictionaryExtensionsTests
     {
         [TestCase]
-        public void InsertTest1()
+        public void InsertProgramToStartTest1()
         {
             Dictionary<int, ProgramToStart> testDict = new Dictionary<int, ProgramToStart>();
             ProgramToStart program1 = new ProgramToStart("test1", "D:\test1.txt");
@@ -21,12 +21,31 @@ namespace Programs_Starter.Models.Tests.Helpers
             testDict.Add(3, program3);
 
             ProgramToStart testProgram = new ProgramToStart("testProgram", "D:\testProgram.txt");
-            testDict.Insert(testProgram, 2);
+            testDict.InsertProgramToStart(testProgram, 2);
 
             Assert.AreEqual(4, testDict.Count);
             Assert.AreEqual(testProgram, testDict[2]);
             Assert.AreEqual(program2, testDict[3]);
             Assert.AreEqual(program3, testDict[4]);
         }
+
+        [TestCase]
+        public void RemoveProgramToStartTest1()
+        {
+            Dictionary<int, ProgramToStart> testDict = new Dictionary<int, ProgramToStart>();
+            ProgramToStart program1 = new ProgramToStart("test1", "D:\test1.txt");
+            testDict.Add(1, program1);
+            ProgramToStart program2 = new ProgramToStart("test2", "D:\test2.txt");
+            testDict.Add(2, program2);
+            ProgramToStart program3 = new ProgramToStart("test3", "D:\test3.txt");
+            testDict.Add(3, program3);
+
+            testDict.RemoveProgramToStart(2);
+            
+            Assert.AreEqual(2, testDict.Count);
+            Assert.AreEqual(program1, testDict[1]);
+            Assert.AreEqual(program3, testDict[2]);
+        }
+        
     }
 }
--- Programs_Starter.Models/Helpers/DictionaryExtensions.cs
+++ Programs_Starter.Models/Helpers/DictionaryExtensions.cs
@@ -13,7 +13,7 @@ namespace Programs_Starter.Models.Helpers
         /// <param name="dict">Dictionary with programs to start</param>
         /// <param name="program">New program to be insterted into dictionary</param>
         /// <param name="index">index on which new program should be inserted</param>
-        public static void Insert(this Dictionary<int, ProgramToStart> dict, ProgramToStart program, int index)
+        public static void InsertProgramToStart(this Dictionary<int, ProgramToStart> dict, ProgramToStart program, int index)
         {
             if (program == null || dict == null)
                 throw new ArgumentNullException();
@@ -37,6 +37,38 @@ namespace Programs_Starter.Models.Helpers
             foreach (var item in tempList.OrderBy(x => x.Key))
             {
                 dict.Add(item.Key + 1, item.Value);
+            }
+
+            tempList.Clear();
+        }
+
+        public static void RemoveProgramToStart(this Dictionary<int, ProgramToStart> dict, int index)
+        {
+            if (dict == null)
+                throw new ArgumentNullException();
+            if (index <= 0)
+                throw new ArgumentException("Index below or equal 0!");
+            if (index > dict.Count)
+                throw new ArgumentOutOfRangeException("Index is bigger then number of objects in dictionary!");
+
+            List<KeyValuePair<int, ProgramToStart>> tempList = new List<KeyValuePair<int, ProgramToStart>>();
+
+            foreach (var item in dict.OrderBy(x => x.Key))
+            {
+                if (item.Key > index)
+                {
+                    tempList.Add(item);
+                    dict.Remove(item.Key);
+                }
+                else if (item.Key == index)
+                {
+                    dict.Remove(item.Key);
+                }
+            }
+
+            foreach (var item in tempList.OrderBy(x => x.Key))
+            {
+                dict.Add(item.Key - 1, item.Value);
             }
 
             tempList.Clear();
