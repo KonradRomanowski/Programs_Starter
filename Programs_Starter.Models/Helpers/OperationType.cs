4eab903cfb718fb15b57d6d1fffee259b2afb340
Konrad Romanowski <k.romanowski.x@wp.pl>
2019-09-24 18:40:16 +0200

    * Added OperationType class

---
+++ Programs_Starter.Models/Helpers/OperationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs_Starter.Models.Helpers
+{
+    public class OperationType
+    {
+        public string Value { get; set; }
+
+        private OperationType(string value) { Value = value; }
+
+        public static OperationType Added { get { return new OperationType("Added"); } }
+        public static OperationType Removed { get { return new OperationType("Removed"); } }
+    }
+}
